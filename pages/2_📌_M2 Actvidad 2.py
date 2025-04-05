96e076f28b9a274983e86db6242247ba990d715f
Maleja <usuario@Maleja.local>
2025-04-05 11:00:26 -0500

    Descripción del cambio

--- pages/2_📌_M2 Actvidad 2.py
+++ pages/2_📌_M2 Actvidad 2.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import io
+from contextlib import redirect_stdout
 
 # Configuración de la página
 st.set_page_config(   
@@ -29,3 +31,50 @@ st.markdown("""
 st.header("Solución")
 
 df = pd.read_csv("static/datasets/estudiantes-colombia.csv")
+
+st.dataframe(df)
+
+st.subheader('Primeras 5 filas del dataset')
+st.write(df.head()) 
+
+st.subheader('Últimas 5 filas del dataset')
+st.write(df.tail()) 
+
+
+st.subheader('Muestra un resumen usando el comando .info')
+
+f = io.StringIO()
+with redirect_stdout(f):
+    df.info()
+s = f.getvalue() 
+
+st.text(s)
+
+st.subheader('Muestra un resumen usando el comando .describe')
+
+st.write(df.describe())
+
+st.subheader ('Nombre, edad, y promedio')
+
+st.write(df[['nombre', 'edad','promedio']])
+
+st.subheader('Seleccion de promedios')
+
+opcion = st.selectbox("Selecciona el rango que deseas explorar", 
+
+    ["5.0 - 3.5", "3.4 - 2.5", "2.5-0.0"])
+
+if opcion == "5.0 - 3.5":
+    # Filtra los estudiantes con promedio entre 5.0 y 3.5
+    resultado = df[(df['promedio'] >= 3.5) & (df['promedio'] <= 5.0)]
+    st.write(resultado)
+
+elif opcion == "3.4 - 2.5":
+    # Filtra los estudiantes con promedio entre 3.4 y 2.5
+    resultado = df[(df['promedio'] >= 2.5) & (df['promedio'] < 3.5)]
+    st.write(resultado)
+    
+elif opcion == "2.5-0.0":
+    # Filtra los estudiantes con promedio entre 2.5 y 0.0
+    resultado = df[(df['promedio'] >= 0.0) & (df['promedio'] < 2.5)]
+    st.write(resultado)
